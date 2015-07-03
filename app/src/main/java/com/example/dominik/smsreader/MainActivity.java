4c4e99e6ab7c0c4ae5afdbd3d47da412e3ee3113
Divolo <208800@student.pwr.edu.pl>
2015-07-03 11:54:55 +0200

    SMS storing in ArrayList<String[]> rather than String[][]

--- app/src/main/java/com/example/dominik/smsreader/MainActivity.java
+++ app/src/main/java/com/example/dominik/smsreader/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.dominik.smsreader;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
@@ -16,7 +23,8 @@ public class MainActivity extends Activity {
     public static final int BODY = 2;
     public static final String SMS_DATA = "SMS_DATA";
 
-    private String [][] smsDataSet;
+    private ArrayList<String[]> smsDataSet;
+    private ArrayList<String[]> workingSmsDataSet;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -31,26 +39,54 @@ public class MainActivity extends Activity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        if(smsDataSet == null) {
+
+        if (smsDataSet == null) {
             Uri uriSMSURI = Uri.parse("content://sms/inbox");
             Cursor cur = getContentResolver().query(uriSMSURI, null, null, null, null);
-            smsDataSet = new String[3][cur.getCount()];
-
+            smsDataSet = new ArrayList<>(cur.getCount());
+            String[] sms;
             while (cur.moveToNext()) {
-                smsDataSet[ADRESS][cur.getPosition()] = cur.getString(cur.getColumnIndex("address"));
-                smsDataSet[DATE][cur.getPosition()] = cur.getString(cur.getColumnIndex("date"));
-                smsDataSet[BODY][cur.getPosition()] = cur.getString(cur.getColumnIndex("body"));
+                sms = new String[3];
+                sms[ADRESS] = cur.getString(cur.getColumnIndex("address"));
+                sms[DATE] = cur.getString(cur.getColumnIndex("date"));
+                sms[BODY] = cur.getString(cur.getColumnIndex("body"));
+                smsDataSet.add(sms);
             }
+            System.out.println("SIZE " + smsDataSet.size());
+            workingSmsDataSet = smsDataSet;
+            handleIntent(getIntent());
+            System.out.println("SIZE2 " + smsDataSet.size());
+            mAdapter = new MyAdapter(smsDataSet);
+            mRecyclerView.setAdapter(mAdapter);
         }
-
-        mAdapter = new MyAdapter(smsDataSet);
-        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        MenuItem search = menu.findItem(R.id.search);
+
+        search.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                Toast.makeText(getApplicationContext(), "Return", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
 
@@ -63,6 +99,20 @@ public class MainActivity extends Activity {
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        smsDataSet = (String[][]) savedInstanceState.getSerializable(SMS_DATA);
+        smsDataSet = (ArrayList<String[]>) savedInstanceState.getSerializable(SMS_DATA);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            System.out.println("HANDLED" + query);
+
+        }
     }
 }
--- app/src/main/java/com/example/dominik/smsreader/MyAdapter.java
+++ app/src/main/java/com/example/dominik/smsreader/MyAdapter.java
@@ -14,7 +14,6 @@ import java.util.Date;
  */
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
-    private String[][] mDataset;
     private ArrayList<String[]> smsData;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -31,8 +30,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         }
     }
 
-    public MyAdapter(String[][] myDataset) {
-        mDataset = myDataset;
+    public MyAdapter(ArrayList<String[]> mySmsData) {
+        System.out.println("SIZE3 " + mySmsData.size());
+        this.smsData = mySmsData;
+        System.out.println("SIZE4 " + this.smsData.size());
     }
 
     // Create new views (invoked by the layout manager)
@@ -50,17 +51,17 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public void onBindViewHolder(ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        Date date = new Date(Long.parseLong(mDataset[MainActivity.DATE][position]));
 
-        holder.adress.setText(mDataset[MainActivity.ADRESS][position]);
+        Date date = new Date(Long.parseLong(smsData.get(position)[MainActivity.DATE]));
+        holder.adress.setText(smsData.get(position)[MainActivity.ADRESS]);
         holder.date.setText(date.toString());
-        holder.body.setText(mDataset[MainActivity.BODY][position]);
+        holder.body.setText(smsData.get(position)[MainActivity.BODY]);
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset[0].length;
+        return smsData.size();
     }
 
 }
